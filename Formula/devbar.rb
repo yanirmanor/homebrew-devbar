f25286b917da0f58d9eda445e82173e1b3f56428
yanirmanor <yanirmanor@gmail.com>
2026-03-01 09:59:48 +0200

    Initial Homebrew formula for DevBar v1.0.0

---
+++ Formula/devbar.rb
@@ -0,0 +1,27 @@
+class Devbar < Formula
+  desc "macOS menu bar app that monitors local dev servers"
+  homepage "https://github.com/yanirmanor/devbar"
+  url "https://github.com/yanirmanor/devbar/archive/refs/tags/v1.0.0.tar.gz"
+  # sha256 "UPDATE_THIS_AFTER_RELEASE"
+  license "MIT"
+
+  depends_on xcode: ["14.0", :build]
+  depends_on :macos
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/DevBar" => "devbar"
+  end
+
+  def caveats
+    <<~EOS
+      DevBar runs as a menu bar app. Start it with:
+        devbar
+      It will appear as a </> icon in your menu bar.
+    EOS
+  end
+
+  test do
+    assert_predicate bin/"devbar", :executable?
+  end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew Tap for DevBar
+
+## Install
+
+```bash
+brew tap yanirmanor/devbar
+brew install devbar
+```
+
+## About
+
+[DevBar](https://github.com/yanirmanor/devbar) is a macOS menu bar app that monitors your local dev servers.
